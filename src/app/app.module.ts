b683d32225c299a82a14af8e8bedfd6888b15598
Din Lanh <langdinhieu@gmail.com>
2019-10-03 01:16:19 -0400

    added bootstrap, header and footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { BasePageComponent } from './partials/base-page/base-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    BasePageComponent
   ],
   imports: [
     BrowserModule,
