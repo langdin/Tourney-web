10e3a7d36bde8d4fc5b93d66de56529dcecf6cac
Din Lanh <langdinhieu@gmail.com>
2019-10-03 01:16:19 -0400

    added bootstrap, header and footer.

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,4 +8,10 @@ let indexController = require('../controllers/index');
 /* POST - processes the Login Page */
 router.post('/login', indexController.processLoginPage);
 
+/* POST - processes the Register Page */
+router.post('/register', indexController.processRegisterPage);
+
+/* GET - perform user logout */
+router.get('/logout', indexController.processRegisterPage);
+
 module.exports = router;
