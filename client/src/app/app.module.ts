d974d0254dfc2adb5af0238a7b855c94cb65bee0
Din Lanh <langdinhieu@gmail.com>
2019-10-03 17:23:35 -0400

    added imports and providers to app.module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,16 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './partials/footer/footer.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { BasePageComponent } from './partials/base-page/base-page.component';
+import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
+import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+
+// Route Guards
+import { AuthGuard } from './guards/auth.guard';
+
+export function jwtTokenGetter() {
+  return localStorage.getItem('id_token');
+}
 
 @NgModule({
   declarations: [
@@ -16,9 +26,16 @@ import { BasePageComponent } from './partials/base-page/base-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FlashMessagesModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: jwtTokenGetter
+      }
+    })
   ],
-  providers: [],
+  providers: [FlashMessagesService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
