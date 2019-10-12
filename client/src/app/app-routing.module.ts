70b49741ce123b259adbd87f6d8518bc34efca4f
Din Lanh <langdinhieu@gmail.com>
2019-10-12 16:25:24 -0400

    added view for add tourney

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { MyTourneysComponent } from './pages/my-tourneys/my-tourneys.component';
+import { MyTourneyDetailsComponent } from './pages/my-tourneys/my-tourney-details/my-tourney-details.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'my_tourneys', component: MyTourneysComponent}
+  {path: 'my_tourneys', component: MyTourneysComponent, canActivate: [AuthGuard]},
+  {path: 'my_tourneys/add', component: MyTourneyDetailsComponent, data: {title: 'Add Tourney'}, canActivate: [AuthGuard]},
+  {path: 'my_tourneys/edit/:id', component: MyTourneyDetailsComponent, data: {title: 'Edit Tourney'}, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
--- client/src/app/services/my-tourneys.service.ts
+++ client/src/app/services/my-tourneys.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Tourney } from '../models/tourney';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ import { Observable } from 'rxjs';
 export class MyTourneysService {
 
 
-  private endpoint = 'http://localhost:3000/tourneys';
+  private endpoint = 'http://localhost:3000/tourneys/';
 
   private httpOptions = {
     headers: new HttpHeaders({
@@ -23,7 +24,14 @@ export class MyTourneysService {
   ) { }
 
   public getUserTourneys(userId: any): Observable<any> {
-    return this.httpClient.post(this.endpoint + '/my', userId, this.httpOptions);
+    return this.httpClient.post(this.endpoint + 'my', userId, this.httpOptions);
   }
 
+  public addTourney(tourney: Tourney): Observable<any> {
+    return this.httpClient.post(this.endpoint + 'add', tourney, this.httpOptions);
+  }
+
+  public updateTourney(tourney: Tourney): Observable<any> {
+    return this.httpClient.post(this.endpoint + 'edit/' + tourney._id, tourney, this.httpOptions);
+  }
 }
