f3205d4d998a59099fc59138f644234c6b666d81
Din Lanh <langdinhieu@gmail.com>
2019-10-13 21:54:23 -0400

    implemented add bout

---
+++ src/app/services/bout.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Bout } from '../models/bout';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoutService {
+
+  private endpoint = 'http://localhost:3000/bouts/';
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    })
+  };
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public getBoutsByTourney(tourneyId: string): Observable<any> {
+    return this.httpClient.get(this.endpoint + 'by_tourney/' + tourneyId, this.httpOptions);
+  }
+
+  public addBout(bout: Bout): Observable<any> {
+    return this.httpClient.post(this.endpoint + 'add', bout, this.httpOptions);
+  }
+}
