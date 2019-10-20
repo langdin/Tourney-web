d18b9505e520db558dc2f6a87cfb99933a292770
Din Lanh <langdinhieu@gmail.com>
2019-10-19 21:28:33 -0400

    implemented navigation to same page = refresh

--- client/src/app/pages/my-tourneys/manage-tourney/manage-tourney.component.ts
+++ client/src/app/pages/my-tourneys/manage-tourney/manage-tourney.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MyTourneysService } from 'src/app/services/my-tourneys.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { Tourney } from 'src/app/models/tourney';
 import { BoutService } from 'src/app/services/bout.service';
 import { Bout } from 'src/app/models/bout';
@@ -16,6 +16,7 @@ export class ManageTourneyComponent implements OnInit {
   tourneyId: string;
   tourney: Tourney;
   bouts: Bout[];
+  mySubscription: any;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -24,7 +25,18 @@ export class ManageTourneyComponent implements OnInit {
     private flashMessage: FlashMessagesService,
     private router: Router
   ) {
-   }
+
+    this.router.routeReuseStrategy.shouldReuseRoute =  () => {
+      return false;
+    };
+
+    this.mySubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Trick the Router into believing it's last link wasn't previously loaded
+        this.router.navigated = false;
+      }
+    });
+  }
 
   ngOnInit() {
     this.tourney = new Tourney();
@@ -56,17 +68,25 @@ export class ManageTourneyComponent implements OnInit {
     bout.maxNumOfPlayers = this.tourney.numberOfPlayers;
     bout.tourneyId = this.tourneyId;
     if (bout.number > 1) {
-      this.flashMessage.show('New bouts can not be added.', {cssClass: 'alert-warning', timeOut: 3000});
+      this.flashMessage.show('New bouts can not be added.', { cssClass: 'alert-warning', timeOut: 3000 });
     } else {
       this.boutService.addBout(bout).subscribe(data => {
         if (data.success) {
-          this.flashMessage.show(data.msg, {cssClass: 'alert-success', timeOut: 3000});
-          location.reload();
+          this.flashMessage.show(data.msg, { cssClass: 'alert-success', timeOut: 3000 });
+
         } else {
-          this.flashMessage.show(data.msg, {cssClass: 'alert-danger', timeOut: 3000});
-          location.reload();
+          this.flashMessage.show(data.msg, { cssClass: 'alert-danger', timeOut: 3000 });
         }
+        this.router.navigate(['/manage_tourney/' + this.tourneyId]);
       });
     }
   }
+
+
+// tslint:disable-next-line: use-life-cycle-interface
+ngOnDestroy() {
+  if (this.mySubscription) {
+    this.mySubscription.unsubscribe();
+  }
+}
 }
