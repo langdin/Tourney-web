f37e670cfc4bb44fc39bfa84564702e421546077
Din Lanh <langdinhieu@gmail.com>
2019-11-16 18:20:41 -0500

    craeted mock home page

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.user = new User();
   }
 
+  isLoggedIn(): boolean {
+    const result = this.authService.loggedIn();
+    if (result) {
+      this.user = JSON.parse(localStorage.getItem('user'));
+    }
+    return result;
+  }
 }
