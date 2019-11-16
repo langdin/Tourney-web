e7746dd888344a5c9260fb3e5f5e1aa55932fa33
Din Lanh <langdinhieu@gmail.com>
2019-11-16 16:51:30 -0500

    removed depricated function

--- server/config/app.js
+++ server/config/app.js
@@ -103,7 +103,7 @@ app.use('/bouts', boutRouter);
 app.use('/tourneys', tourneyRouter);
 //app.use('/api/contact-list', passport.authenticate('jwt', {session: false}), contactRouter);
 app.get('*', (req, res) => {
-  res.sendfile(path.join(__dirname, '../../public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 // catch 404 and forward to error handler
