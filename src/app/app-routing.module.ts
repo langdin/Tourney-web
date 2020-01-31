6d14e4bace8fb75e1f64d3539a24fec71fa9d252
Din Lanh <langdinhieu@gmail.com>
2020-01-31 00:58:58 -0500

    removed my-tourney-delete component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { MyTourneysComponent } from './pages/my-tourneys/my-tourneys.component';
-import { MyTourneyDetailsComponent } from './pages/my-tourneys/my-tourney-details/my-tourney-details.component';
 import { AuthGuard } from './guards/auth.guard';
-import { MyTourneyDeleteComponent } from './pages/my-tourneys/my-tourney-delete/my-tourney-delete.component';
 import { ManageTourneyComponent } from './pages/my-tourneys/manage-tourney/manage-tourney.component';
 import { ManageBoutComponent } from './pages/bout/manage-bout/manage-bout.component';
 import { PlayerDetailsComponent } from './pages/player/player-details/player-details.component';
@@ -32,7 +30,6 @@ const routes: Routes = [
   {path: 'view_tourney/:id', component: ViewTourneyComponent},
 
   {path: 'my_tourneys', component: MyTourneysComponent, canActivate: [AuthGuard]},
-  {path: 'my_tourneys/delete/:id', component: MyTourneyDeleteComponent, canActivate: [AuthGuard]},
 
   {path: 'manage_tourney/:id', component: ManageTourneyComponent, canActivate: [AuthGuard]},
 
